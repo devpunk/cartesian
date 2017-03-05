e97b2bec77002f90b5c27926ebc13642056452a2
vauxhall <zero.griffin@gmail.com>
2017-03-04 18:43:28 -0600

    draw list header and cell

--- cartesian/Controller/Draw/CDrawList.swift
+++ cartesian/Controller/Draw/CDrawList.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CDrawList:CController
 {
+    private weak var viewList:VDrawList!
     
+    override func loadView()
+    {
+        let viewList:VDrawList = VDrawList(controller:self)
+        self.viewList = viewList
+        view = viewList
+    }
 }
---
+++ cartesian/View/DrawList/VDrawListCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VDrawListCell:UICollectionViewCell
+{
+    
+}
