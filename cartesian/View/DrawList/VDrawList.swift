8190a43334b494ea4554bad2db8cd5c3526cb5f0
vauxhall <zero.griffin@gmail.com>
2017-03-04 18:52:49 -0600

    collection view delegate

--- cartesian/View/DrawList/VDrawList.swift
+++ cartesian/View/DrawList/VDrawList.swift
@@ -1,6 +1,37 @@
 import UIKit
 
-class VDrawList:VView
+class VDrawList:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    private weak var controller:CDrawList!
+    private let kHeaderHeight:CGFloat = 60
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CDrawList
+        
+        
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        return VDrawListCell()
+    }
 }
