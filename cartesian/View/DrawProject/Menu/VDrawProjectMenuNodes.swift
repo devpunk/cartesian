85b2021672c05e8838078204455de1c1a44fbaaa
vauxhall <zero.griffin@gmail.com>
2017-03-06 09:19:50 -0600

    view menu nodes

---
+++ cartesian/View/DrawProject/Menu/VDrawProjectMenuNodes.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class VDrawProjectMenuNodes:UIView
+{
+    private weak var controller:CDrawProject!
+    
+    init(controller:CDrawProject)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
