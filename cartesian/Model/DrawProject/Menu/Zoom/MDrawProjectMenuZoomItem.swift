ba3f301c98b3da1b51b5791ecc7e6998cfdee902
vauxhall <zero.griffin@gmail.com>
2017-03-08 12:55:09 -0600

    model zoom

--- cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoomItem.swift
+++ cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoomItem.swift
@@ -2,5 +2,10 @@ import UIKit
 
 class MDrawProjectMenuZoomItem
 {
+    let scalar:CGFloat
     
+    init(scalar:CGFloat)
+    {
+        self.scalar = scalar
+    }
 }
---
+++ cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoomItem100.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MDrawProjectMenuZoomItem100:MDrawProjectMenuZoomItem
+{
+    private let kScalar:CGFloat = 1
+    
+    init()
+    {
+        super.init(scalar:kScalar)
+    }
+}
