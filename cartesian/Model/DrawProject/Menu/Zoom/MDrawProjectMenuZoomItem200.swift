379701eb0c4dbb0e1ed76629a8b7551af01d2f22
vauxhall <zero.griffin@gmail.com>
2017-03-08 12:56:45 -0600

    zoom items 50 and 200

---
+++ cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoomItem200.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MDrawProjectMenuZoomItem200:MDrawProjectMenuZoomItem
+{
+    private let kScalar:CGFloat = 2
+    
+    init()
+    {
+        super.init(scalar:kScalar)
+    }
+}
---
+++ cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoomItem50.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MDrawProjectMenuZoomItem50:MDrawProjectMenuZoomItem
+{
+    private let kScalar:CGFloat = 0.5
+    
+    init()
+    {
+        super.init(scalar:kScalar)
+    }
+}
