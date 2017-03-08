6d4f130c3e8ac5ef8b8ae55df30a51ad9a260fdc
vauxhall <zero.griffin@gmail.com>
2017-03-08 10:02:43 -0600

    model menu edit bar

--- cartesian/View/DrawProject/Menu/VDrawProjectMenuEditBar.swift
+++ cartesian/View/DrawProject/Menu/VDrawProjectMenuEditBar.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class VDrawProjectMenuEditBar:UIView
+class VDrawProjectMenuEditBar:UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CDrawProject!
+    private weak var collectionView:VCollection!
     private let kBorderHeight:CGFloat = 1
     
     init(controller:CDrawProject)
@@ -15,7 +16,21 @@ class VDrawProjectMenuEditBar:UIView
         
         let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.1))
         
+        let collectionView:VCollection = VCollection()
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VDrawProjectMenuEditBarCell.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        }
+        
         addSubview(border)
+        addSubview(collectionView)
         
         NSLayoutConstraint.topToTop(
             view:border,
@@ -26,10 +41,26 @@ class VDrawProjectMenuEditBar:UIView
         NSLayoutConstraint.equalsHorizontal(
             view:border,
             toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return
     }
 }
