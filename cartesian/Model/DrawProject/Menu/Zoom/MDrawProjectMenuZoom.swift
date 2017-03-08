001b57795153d4e40bb136126a7327e060b939ef
vauxhall <zero.griffin@gmail.com>
2017-03-08 12:59:11 -0600

    factory zoom

--- cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoom.swift
+++ cartesian/Model/DrawProject/Menu/Zoom/MDrawProjectMenuZoom.swift
@@ -2,49 +2,30 @@ import UIKit
 
 class MDrawProjectMenuZoom
 {
-    let items:[CGFloat]
+    let items:[MDrawProjectMenuZoomItem]
     var currentItem:Int
-    private let kResourceName:String = "ResourceZoom"
-    private let kResourceExtension:String = "plist"
-    private let kInitialSelected:Int = 4
-    private let kNoListDefaultItem:CGFloat = 1
     
     init()
     {
-        guard
-            
-            let resourceUrl:URL = Bundle.main.url(
-                forResource:kResourceName,
-                withExtension:kResourceExtension),
-            let zoomArray:NSArray = NSArray(
-                contentsOf:resourceUrl),
-            let items:[CGFloat] = zoomArray as? [CGFloat]
-            
-        else
-        {
-            self.items = [kNoListDefaultItem]
-            currentItem = 0
-            
-            return
-        }
+        let item50:MDrawProjectMenuZoomItem50 = MDrawProjectMenuZoomItem50()
+        let item100:MDrawProjectMenuZoomItem100 = MDrawProjectMenuZoomItem100()
+        let item200:MDrawProjectMenuZoomItem200 = MDrawProjectMenuZoomItem200()
+        
+        var items:[MDrawProjectMenuZoomItem] = []
+        items.append(item50)
+        
+        currentItem = items.count
+        items.append(item100)
+        items.append(item200)
         
         self.items = items
-        
-        if items.count > kInitialSelected
-        {
-            currentItem = kInitialSelected
-        }
-        else
-        {
-            currentItem = 0
-        }
     }
     
     //MARK: public
     
     func currentZoom() -> CGFloat
     {
-        let zoom:CGFloat = items[currentItem]
+        let zoom:CGFloat = items[currentItem].scalar
         
         return zoom
     }
