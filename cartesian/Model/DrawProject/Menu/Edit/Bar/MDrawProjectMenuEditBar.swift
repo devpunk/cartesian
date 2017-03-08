487280b669e58a87ecae0ec9255b84a979b066bc
vauxhall <zero.griffin@gmail.com>
2017-03-08 17:13:29 -0600

    resize assets

--- cartesian/Model/DrawProject/Menu/Edit/Bar/MDrawProjectMenuEditBar.swift
+++ cartesian/Model/DrawProject/Menu/Edit/Bar/MDrawProjectMenuEditBar.swift
@@ -12,7 +12,7 @@ class MDrawProjectMenuEditBar
         
         items = [
             itemColor,
-            itemTrash,
-            itemResize]
+            itemResize,
+            itemTrash]
     }
 }
