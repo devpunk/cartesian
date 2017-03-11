f8002dbb9b73647efe0a5d43c6cacb28b14a935a
vauxhall <zero.griffin@gmail.com>
2017-03-11 15:17:54 -0600

    slider in rotation

--- cartesian/View/DrawProject/Rotate/VDrawProjectRotate.swift
+++ cartesian/View/DrawProject/Rotate/VDrawProjectRotate.swift
@@ -4,12 +4,16 @@ class VDrawProjectRotate:UIView
 {
     private weak var controller:CDrawProject!
     private weak var node:DNode?
+    private weak var slider:UISlider!
     private weak var layoutDoneLeft:NSLayoutConstraint!
     private weak var layoutResetLeft:NSLayoutConstraint!
     private let kAnimationDuration:TimeInterval = 0.3
     private let kButtonWidth:CGFloat = 120
     private let kButtonHeight:CGFloat = 36
     private let kButtonBottom:CGFloat = -20
+    private let kSliderMargin:CGFloat = 15
+    private let kSliderHeight:CGFloat = 50
+    private let kMaxValue:Float = 180
     
     init(
         controller:CDrawProject,
@@ -36,6 +40,10 @@ class VDrawProjectRotate:UIView
             NSLocalizedString("VDrawProjectRotate_buttonReset", comment:""),
             for:UIControlState.normal)
         buttonReset.titleLabel!.font = UIFont.regular(size:14)
+        buttonReset.addTarget(
+            self,
+            action:#selector(actionReset(sender:)),
+            for:UIControlEvents.touchUpInside)
         
         let buttonDone:UIButton = UIButton()
         buttonDone.translatesAutoresizingMaskIntoConstraints = false
@@ -52,10 +60,25 @@ class VDrawProjectRotate:UIView
             for:UIControlState.normal)
         buttonDone.titleLabel!.font = UIFont.bold(size:14)
         buttonDone.layer.cornerRadius = kButtonHeight / 2.0
+        buttonDone.addTarget(
+            self,
+            action:#selector(actionDone(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        let slider:UISlider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.minimumTrackTintColor = UIColor.cartesianBlue
+        slider.maximumTrackTintColor = UIColor.cartesianBlue
+        slider.minimumValue = -kMaxValue
+        slider.maximumValue = kMaxValue
+        slider.isContinuous = true
+        slider.value = node.rotation
+        self.slider = slider
         
         addSubview(blur)
         addSubview(buttonDone)
         addSubview(buttonReset)
+        addSubview(slider)
         
         NSLayoutConstraint.equals(
             view:blur,
@@ -87,6 +110,17 @@ class VDrawProjectRotate:UIView
         layoutResetLeft = NSLayoutConstraint.leftToLeft(
             view:buttonReset,
             toView:self)
+        
+        NSLayoutConstraint.bottomToTop(
+            view:slider,
+            toView:buttonReset)
+        NSLayoutConstraint.height(
+            view:slider,
+            constant:kSliderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:slider,
+            toView:self,
+            margin:kSliderMargin)
     }
     
     required init?(coder:NSCoder)
@@ -103,6 +137,18 @@ class VDrawProjectRotate:UIView
         layoutResetLeft.constant = buttonMargin
         
         super.layoutSubviews()
+    }
+    
+    //MARK: actions
+    
+    func actionDone(sender button:UIButton)
+    {
+        animateHide()
+    }
+    
+    func actionReset(sender button:UIButton)
+    {
+        slider.value = 0
     }
     
     //MARK: private
