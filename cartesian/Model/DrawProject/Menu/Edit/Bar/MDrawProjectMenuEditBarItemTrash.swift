36f04968248636e2575fd27bad073642d967c6f7
vauxhall <zero.griffin@gmail.com>
2017-03-12 14:29:07 -0600

    trash alert controller

--- cartesian/Model/DrawProject/Menu/Edit/Bar/MDrawProjectMenuEditBarItemTrash.swift
+++ cartesian/Model/DrawProject/Menu/Edit/Bar/MDrawProjectMenuEditBarItemTrash.swift
@@ -13,7 +13,53 @@ class MDrawProjectMenuEditBarItemTrash:MDrawProjectMenuEditBarItem
     
     override func selected(controller:CDrawProject)
     {
+        UIApplication.shared.keyWindow!.endEditing(true)
         
+        let alert:UIAlertController = UIAlertController(
+            title:NSLocalizedString("MDrawProjectMenuEditBarItemTrash_alertTitle", comment:""),
+            message:nil,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("MDrawProjectMenuEditBarItemTrash_alertCancel", comment:""),
+            style:
+            UIAlertActionStyle.cancel)
+        
+        let actionDelete:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("MDrawProjectMenuEditBarItemTrash_alertDelete", comment:""),
+            style:
+            UIAlertActionStyle.destructive)
+        { (action:UIAlertAction) in
+            
+            DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+            { [weak self, weak controller] in
+                
+                guard
+                
+                    let node:DNode = controller?.editingNode?.viewSpatial.model
+                
+                else
+                {
+                    return
+                }
+                
+                self?.confirmDelete(node:node)
+            }
+        }
+        
+        alert.addAction(actionDelete)
+        alert.addAction(actionCancel)
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = controller.viewProject
+            popover.sourceRect = CGRect.zero
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+        }
+        
+        controller.present(alert, animated:true, completion:nil)
     }
     
     //MARK: private
