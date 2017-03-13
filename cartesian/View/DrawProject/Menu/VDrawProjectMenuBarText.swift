0e40dcb05000e17775086de880b5069c55572264
vauxhall <zero.griffin@gmail.com>
2017-03-13 14:47:07 -0600

    text send button

--- cartesian/View/DrawProject/Menu/VDrawProjectMenuBarText.swift
+++ cartesian/View/DrawProject/Menu/VDrawProjectMenuBarText.swift
@@ -11,9 +11,6 @@ class VDrawProjectMenuBarText:UIView
     private let kContentHeight:CGFloat = 32
     private let kBaseWidth:CGFloat = 200
     private let kButtonWidth:CGFloat = 60
-    private let kButtonRight:CGFloat = -10
-    private let kBaseRight:CGFloat = -10
-    private let kButtonCornerRadius:CGFloat = 6
     private let kInsets:CGFloat = 5
     
     init(controller:CDrawProject)
@@ -35,19 +32,16 @@ class VDrawProjectMenuBarText:UIView
         
         let button:UIButton = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor.cartesianBlue
         button.clipsToBounds = true
-        button.setTitleColor(
-            UIColor.white,
+        button.setImage(
+            #imageLiteral(resourceName: "assetGenericSend").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
             for:UIControlState.normal)
-        button.setTitleColor(
-            UIColor(white:1, alpha:0.2),
+        button.setImage(
+            #imageLiteral(resourceName: "assetGenericSend").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
             for:UIControlState.highlighted)
-        button.setTitle(
-            NSLocalizedString("VDrawProjectMenuBarText_button", comment:""),
-            for:UIControlState.normal)
-        button.titleLabel!.font = UIFont.bold(size:14)
-        button.layer.cornerRadius = kButtonCornerRadius
+        button.imageView!.clipsToBounds = true
+        button.imageView!.contentMode = UIViewContentMode.center
+        button.imageView!.tintColor = UIColor(white:0, alpha:0.1)
         
         let textView:UITextView = UITextView()
         textView.clipsToBounds = true
@@ -84,8 +78,7 @@ class VDrawProjectMenuBarText:UIView
             constant:kContentHeight)
         NSLayoutConstraint.rightToLeft(
             view:viewBase,
-            toView:button,
-            constant:kBaseRight)
+            toView:button)
         NSLayoutConstraint.width(
             view:viewBase,
             constant:kBaseWidth)
@@ -98,8 +91,7 @@ class VDrawProjectMenuBarText:UIView
             constant:kContentHeight)
         NSLayoutConstraint.rightToRight(
             view:button,
-            toView:self,
-            constant:kButtonRight)
+            toView:self)
         NSLayoutConstraint.width(
             view:button,
             constant:kButtonWidth)
