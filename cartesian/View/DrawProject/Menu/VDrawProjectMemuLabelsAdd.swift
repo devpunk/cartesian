62cb55abb81e463966f9976304ea6cf758410c95
vauxhall <zero.griffin@gmail.com>
2017-03-14 09:15:34 -0600

    labels add

---
+++ cartesian/View/DrawProject/Menu/VDrawProjectMemuLabelsAdd.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class VDrawProjectMenuLabelsAdd:UIButton
+{
+    private weak var controller:CDrawProject!
+    
+    init(controller:CDrawProject)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
