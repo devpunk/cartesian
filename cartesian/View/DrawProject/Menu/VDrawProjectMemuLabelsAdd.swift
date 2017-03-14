d4baa1c8bb87e45ff9cf26c4747c0b647377cdce
vauxhall <zero.griffin@gmail.com>
2017-03-14 09:31:10 -0600

    button add hover

--- cartesian/View/DrawProject/Menu/VDrawProjectMemuLabelsAdd.swift
+++ cartesian/View/DrawProject/Menu/VDrawProjectMemuLabelsAdd.swift
@@ -3,6 +3,9 @@ import UIKit
 class VDrawProjectMenuLabelsAdd:UIButton
 {
     private weak var controller:CDrawProject!
+    private let kIconWidth:CGFloat = 60
+    private let kAlphaSelected:CGFloat = 0.2
+    private let kAlphaNotSelected:CGFloat = 1
     
     init(controller:CDrawProject)
     {
@@ -10,11 +13,90 @@ class VDrawProjectMenuLabelsAdd:UIButton
         clipsToBounds = true
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
+        addTarget(
+            self,
+            action:#selector(actionButton(sender:)),
+            for:UIControlEvents.touchUpInside)
         self.controller = controller
+        
+        let icon:UIImageView = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.isUserInteractionEnabled = false
+        icon.clipsToBounds = true
+        icon.image = #imageLiteral(resourceName: "assetGenericAddSimple")
+        icon.contentMode = UIViewContentMode.center
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.bold(size:14)
+        label.textColor = UIColor.black
+        label.text = NSLocalizedString("VDrawProjectMenuLabelsAdd_label", comment:"")
+        
+        addSubview(icon)
+        addSubview(label)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:icon,
+            constant:kIconWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.rightToLeft(
+            view:label,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: actions
+    
+    func actionButton(sender button:UIButton)
+    {
+        
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            alpha = kAlphaSelected
+        }
+        else
+        {
+            alpha = kAlphaNotSelected
+        }
     }
 }
