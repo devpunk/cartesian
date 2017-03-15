0ad1978de26abd624c75819dba10bf54fca80e49
vauxhall <zero.griffin@gmail.com>
2017-03-15 11:03:09 -0600

    parsing font resource

--- cartesian/Model/DrawProject/Menu/Labels/MDrawProjectMenuLabelsFont.swift
+++ cartesian/Model/DrawProject/Menu/Labels/MDrawProjectMenuLabelsFont.swift
@@ -3,11 +3,33 @@ import Foundation
 class MDrawProjectMenuLabelsFont
 {
     let items:[MDrawProjectMenuLabelsFontItem]
+    private let kResourceName:String = "ResourceFont"
+    private let kResourceExtension:String = "plist"
     
     init()
     {
+        guard
+            
+            let resourceFont:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension),
+            let fontArray:NSArray = NSArray(
+                contentsOf:resourceFont),
+            let fontList:[AnyObject] = fontArray as? [AnyObject]
+            
+        else
+        {
+            self.items = []
+            
+            return
+        }
+        
         var items:[MDrawProjectMenuLabelsFontItem] = []
         
+        for font:AnyObject in fontList
+        {
+            
+        }
         
         self.items = items
     }
