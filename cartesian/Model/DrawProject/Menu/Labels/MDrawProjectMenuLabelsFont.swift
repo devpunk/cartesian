e06c95b4742d069341f0fa91a03a8f12f34bf5e9
vauxhall <zero.griffin@gmail.com>
2017-03-15 10:56:18 -0600

    refactor model fonts

--- cartesian/Model/DrawProject/Menu/Labels/MDrawProjectMenuLabelsFont.swift
+++ cartesian/Model/DrawProject/Menu/Labels/MDrawProjectMenuLabelsFont.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class MDrawProjectMenuLabelsFont
 {
+    let items:[MDrawProjectMenuLabelsFontItem]
     
+    init()
+    {
+        var items:[MDrawProjectMenuLabelsFontItem] = []
+        
+        
+        self.items = items
+    }
 }
--- cartesian/Model/DrawProject/Menu/Labels/MDrawProjectMenuLabelsFontItemTypeItalic.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MDrawProjectMenuLabelsFontItemTypeItalic.swift
-//  cartesian
-//
-//  Created by zero on 3/15/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
-import Foundation
