af8dadc7b5df693c3bfe3425f12ff43447817fd7
vauxhall <zero.griffin@gmail.com>
2017-03-16 16:11:00 -0600

    model type and indexpath

--- cartesian/Model/DrawProject/Font/MDrawProjectFontType.swift
+++ cartesian/Model/DrawProject/Font/MDrawProjectFontType.swift
@@ -3,25 +3,53 @@ import Foundation
 class MDrawProjectFontType
 {
     let items:[MDrawProjectFontTypeItem]
+    let indexPath:IndexPath?
     
     init(model:MDrawProjectMenuLabelsFontItem)
     {
         var items:[MDrawProjectFontTypeItem] = []
+        var index:Int?
         
-        if model.typeRegular != nil
+        if let typeRegular:String = model.typeRegular
         {
             let itemRegular:MDrawProjectFontTypeItemRegular = MDrawProjectFontTypeItemRegular()
             items.append(itemRegular)
+            
+            if typeRegular == model.currentType
+            {
+                index = 0
+            }
         }
-        else if model.typeBold != nil
+        
+        if let typeBold:String = model.typeBold
         {
             let itemBold:MDrawProjectFontTypeItemBold = MDrawProjectFontTypeItemBold()
             items.append(itemBold)
+            
+            if typeBold == model.currentType
+            {
+                index = 1
+            }
         }
-        else if model.typeItalic != nil
+        
+        if let typeItalic:String = model.typeItalic
         {
             let itemItalic:MDrawProjectFontTypeItemItalic = MDrawProjectFontTypeItemItalic()
             items.append(itemItalic)
+            
+            if typeItalic == model.currentType
+            {
+                index = 2
+            }
+        }
+        
+        if let index:Int = index
+        {
+            indexPath = IndexPath(item:index, section:0)
+        }
+        else
+        {
+            indexPath = nil
         }
         
         self.items = items
