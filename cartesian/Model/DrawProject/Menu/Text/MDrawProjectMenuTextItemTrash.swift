59333dac9e498f8e49d6b9449a1b0911244f556a
vauxhall <zero.griffin@gmail.com>
2017-03-18 15:06:34 -0600

    trash text

--- cartesian/Model/DrawProject/Menu/Text/MDrawProjectMenuTextItemTrash.swift
+++ cartesian/Model/DrawProject/Menu/Text/MDrawProjectMenuTextItemTrash.swift
@@ -13,5 +13,39 @@ class MDrawProjectMenuTextItemTrash:MDrawProjectMenuTextItem
     
     override func selected(controller:CDrawProject)
     {
+        UIApplication.shared.keyWindow!.endEditing(true)
+        
+        let alert:UIAlertController = UIAlertController(
+            title:NSLocalizedString("MDrawProjectMenuTextItemTrash_alertTitle", comment:""),
+            message:nil,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("MDrawProjectMenuTextItemTrash_alertCancel", comment:""),
+            style:
+            UIAlertActionStyle.cancel)
+        
+        let actionDelete:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("MDrawProjectMenuTextItemTrash_alertDelete", comment:""),
+            style:
+            UIAlertActionStyle.destructive)
+        { [weak controller] (action:UIAlertAction) in
+            
+            controller?.trashEditing()
+        }
+        
+        alert.addAction(actionDelete)
+        alert.addAction(actionCancel)
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = controller.viewProject
+            popover.sourceRect = CGRect.zero
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+        }
+        
+        controller.present(alert, animated:true, completion:nil)
     }
 }
