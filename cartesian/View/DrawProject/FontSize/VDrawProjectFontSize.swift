e41c08a2c76abaa3877d231f4956a0c59175e244
vauxhall <zero.griffin@gmail.com>
2017-03-19 14:43:55 -0600

    view font size

--- cartesian/View/DrawProject/FontSize/VDrawProjectFontSize.swift
+++ cartesian/View/DrawProject/FontSize/VDrawProjectFontSize.swift
@@ -2,5 +2,82 @@ import UIKit
 
 class VDrawProjectFontSize:UIView
 {
+    private weak var controller:CDrawProject!
+    private weak var delegate:MDrawProjectFontSizeDelegate?
+    private weak var blurContainer:UIView!
+    private weak var layoutBaseTop:NSLayoutConstraint!
+    private let kBaseHeight:CGFloat = 200
     
+    init(
+        controller:CDrawProject,
+        delegate:MDrawProjectFontSizeDelegate)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+        self.controller = controller
+        self.delegate = delegate
+        
+        let blur:VBlur = VBlur.dark()
+        
+        let blurContainer:UIView = UIView()
+        blurContainer.isUserInteractionEnabled = false
+        blurContainer.clipsToBounds = true
+        blurContainer.translatesAutoresizingMaskIntoConstraints = false
+        blurContainer.alpha = 0
+        self.blurContainer = blurContainer
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.clear
+        button.addTarget(
+            self,
+            action:#selector(actionClose(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        let baseView:UIView = UIView()
+        baseView.translatesAutoresizingMaskIntoConstraints = false
+        baseView.clipsToBounds = true
+        baseView.backgroundColor = UIColor.white
+        
+        blurContainer.addSubview(blur)
+        addSubview(blurContainer)
+        addSubview(button)
+        addSubview(baseView)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:blurContainer)
+        
+        NSLayoutConstraint.equals(
+            view:blurContainer,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+        
+        layoutBaseTop = NSLayoutConstraint.topToBottom(
+            view:baseView,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:baseView,
+            constant:kBaseHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:baseView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: actions
+    
+    func actionClose(sender button:UIButton)
+    {
+        animateClose()
+    }
 }
