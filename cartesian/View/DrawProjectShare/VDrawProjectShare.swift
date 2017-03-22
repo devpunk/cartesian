523fc209b6f45fcfe22aff63fb54943f4fa45c6d
vauxhall <zero.griffin@gmail.com>
2017-03-22 16:01:55 -0600

    controller ->  view share

--- cartesian/View/DrawProjectShare/VDrawProjectShare.swift
+++ cartesian/View/DrawProjectShare/VDrawProjectShare.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class VDrawProjectShare:VView
 {
+    private weak var controller:CDrawProjectShare!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        self.controller = controller as? CDrawProjectShare
+        
+        let blur:VBlur = VBlur.extraLight()
+        
+        addSubview(blur)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
