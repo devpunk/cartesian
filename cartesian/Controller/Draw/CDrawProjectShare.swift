404bba6dc67d5b625063f786e2fa20110781f037
vauxhall <zero.griffin@gmail.com>
2017-03-22 15:59:23 -0600

    view draw project share

--- cartesian/Controller/Draw/CDrawProjectShare.swift
+++ cartesian/Controller/Draw/CDrawProjectShare.swift
@@ -2,5 +2,21 @@ import UIKit
 
 class CDrawProjectShare:CController
 {
+    private weak var model:DProject!
     
+    init(model:DProject)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func loadView()
+    {
+        
+    }
 }
