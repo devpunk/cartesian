435f3537437567a610accb0db702ac5ffa61f0b2
vauxhall <zero.griffin@gmail.com>
2017-03-23 15:22:02 -0600

    settings collection view

--- cartesian/View/Settings/VSettings.swift
+++ cartesian/View/Settings/VSettings.swift
@@ -4,11 +4,37 @@ class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
 {
     private weak var controller:CSettings!
     private weak var collectionView:VCollection!
-    private let kHeaderHeight:CGFloat = 200
+    private let kHeaderHeight:CGFloat = 250
+    private let kCollectionBottom:CGFloat = 20
     
     override init(controller:CController)
     {
         super.init(controller:controller)
+        self.controller = controller as? CSettings
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.alwaysBounceVertical = true
+        collectionView.registerHeader(header:VSettingsHeader.self)
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.headerReferenceSize = CGSize(
+                width:0,
+                height:kHeaderHeight)
+            flow.sectionInset = UIEdgeInsets(
+                top:0,
+                left:0,
+                bottom:kCollectionBottom,
+                right:0)
+        }
+        
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -26,6 +52,17 @@ class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
     {
         return 0
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, viewForSupplementaryElementOfKind kind:String, at indexPath:IndexPath) -> UICollectionReusableView
+    {
+        let header:VSettingsHeader = collectionView.dequeueReusableSupplementaryView(
+            ofKind:kind,
+            withReuseIdentifier:
+            VSettingsHeader.reusableIdentifier,
+            for:indexPath) as! VSettingsHeader
+        
+        return header
     }
     
     func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
