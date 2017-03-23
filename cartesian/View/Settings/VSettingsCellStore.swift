efc92499ab97c65b421c56a98d90d574386532d9
vauxhall <zero.griffin@gmail.com>
2017-03-23 16:43:11 -0600

    image store

--- cartesian/View/Settings/VSettingsCellStore.swift
+++ cartesian/View/Settings/VSettingsCellStore.swift
@@ -3,7 +3,8 @@ import UIKit
 class VSettingsCellStore:VSettingsCell
 {
     private let kLabelLeft:CGFloat = 10
-    private let kLabelWidth:CGFloat = 200
+    private let kLabelWidth:CGFloat = 220
+    private let kImageWidth:CGFloat = 60
     
     override init(frame:CGRect)
     {
@@ -13,11 +14,19 @@ class VSettingsCellStore:VSettingsCell
         label.isUserInteractionEnabled = false
         label.backgroundColor = UIColor.clear
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.bold(size:14)
+        label.font = UIFont.bold(size:15)
         label.textColor = UIColor.black
         label.text = NSLocalizedString("VSettingsCellStore_label", comment:"")
         
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.image = #imageLiteral(resourceName: "assetGenericStore")
+        
         addSubview(label)
+        addSubview(imageView)
         
         NSLayoutConstraint.equalsVertical(
             view:label,
@@ -29,6 +38,16 @@ class VSettingsCellStore:VSettingsCell
         NSLayoutConstraint.width(
             view:label,
             constant:kLabelWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageView,
+            constant:kImageWidth)
     }
     
     required init?(coder:NSCoder)
