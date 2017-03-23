553c1177c5b239e47fd03571a8100d08df26327a
vauxhall <zero.griffin@gmail.com>
2017-03-23 15:06:17 -0600

    collection view delegate

--- cartesian/View/Settings/VSettings.swift
+++ cartesian/View/Settings/VSettings.swift
@@ -1,8 +1,10 @@
 import UIKit
 
-class VSettings:VView
+class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CSettings!
+    private weak var collectionView:VCollection!
+    private let kHeaderHeight:CGFloat = 200
     
     override init(controller:CController)
     {
@@ -12,5 +14,22 @@ class VSettings:VView
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        return UICollectionViewCell()
     }
 }
--- cartesian/View/Settings/VSettingsHeader.swift
+++ cartesian/View/Settings/VSettingsHeader.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class VSettingsHeader:UICollectionReusableView
 {
+    private let kLabelHeight:CGFloat = 30
+    private let kLabelBottom:CGFloat = -20
+    private let kVersionKey:String = "CFBundleShortVersionString"
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
@@ -11,6 +15,47 @@ class VSettingsHeader:UICollectionReusableView
         
         let imageView:UIImageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        imageView.image = #imageLiteral(resourceName: "assetGenericLogo")
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.numeric(size:16)
+        label.textAlignment = NSTextAlignment.center
+        
+        addSubview(imageView)
+        addSubview(label)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:label,
+            toView:self,
+            constant:kLabelBottom)
+        NSLayoutConstraint.height(
+            view:label,
+            constant:kLabelHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:label,
+            toView:self)
+        
+        guard
+            
+            let bundleDictionary:[String:Any] = Bundle.main.infoDictionary
+            
+        else
+        {
+            return
+        }
+        
+        let versionString:String? = bundleDictionary[kVersionKey] as? String
+        label.text = versionString
     }
     
     required init?(coder:NSCoder)
