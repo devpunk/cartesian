4031616ca8fe2d454ecf0277face8e1bcf9386c8
vauxhall <zero.griffin@gmail.com>
2017-03-23 17:40:00 -0600

    dsettings became active

--- cartesian/Data/DSettingsExtension.swift
+++ cartesian/Data/DSettingsExtension.swift
@@ -3,5 +3,14 @@ import CoreData
 
 extension DSettings
 {
+    private static let kDeltaTtl:Int16 = 1
     
+    //MARK: public
+    
+    func becameActive()
+    {
+        ttl += DSettings.kDeltaTtl
+        
+        DManager.sharedInstance?.save()
+    }
 }
