0ccefc1580dba2be5beb8150f46cf0d7c622fc3f
vauxhall <zero.griffin@gmail.com>
2017-03-22 18:08:39 -0600

    render link

--- cartesian/Model/DrawProjectShare/MDrawProjectShareRender.swift
+++ cartesian/Model/DrawProjectShare/MDrawProjectShareRender.swift
@@ -63,4 +63,64 @@ class MDrawProjectShareRender
             zoom:kZoom,
             selected:kSelected)
     }
+    
+    class func renderLink(link:DLink, context:CGContext)
+    {
+        guard
+            
+            let nodeOrigin:DNode = link.origin,
+            let nodeDestination:DNode = link.destination
+            
+        else
+        {
+            return
+        }
+        
+        let margin2:CGFloat = kMargin + kMargin
+        let originX:CGFloat = CGFloat(nodeOrigin.centerX)
+        let originY:CGFloat = CGFloat(nodeOrigin.centerY)
+        let originWidth:CGFloat = CGFloat(nodeOrigin.width)
+        let originHeight:CGFloat = CGFloat(nodeOrigin.height)
+        let originWidth_2:CGFloat = originWidth / 2.0
+        let originHeight_2:CGFloat = originHeight / 2.0
+        let originMinX:CGFloat = originX - originWidth_2
+        let originMinY:CGFloat = originY - originHeight_2
+        let originMaxX:CGFloat = originX + originWidth_2
+        let originMaxY:CGFloat = originY + originHeight_2
+        
+        let destinationX:CGFloat = CGFloat(nodeDestination.centerX)
+        let destinationY:CGFloat = CGFloat(nodeDestination.centerY)
+        let destinationWidth:CGFloat = CGFloat(nodeDestination.width)
+        let destinationHeight:CGFloat = CGFloat(nodeDestination.height)
+        let destinationWidth_2:CGFloat = destinationWidth / 2.0
+        let destinationHeight_2:CGFloat = destinationHeight / 2.0
+        let destinationMinX:CGFloat = destinationX - destinationWidth_2
+        let destinationMinY:CGFloat = destinationY - destinationHeight_2
+        let destinationMaxX:CGFloat = destinationX + destinationWidth_2
+        let destinationMaxY:CGFloat = destinationY + destinationHeight_2
+        
+        let minX:CGFloat = min(originMinX, destinationMinX)
+        let maxX:CGFloat = max(originMaxX, destinationMaxX)
+        let minY:CGFloat = min(originMinY, destinationMinY)
+        let maxY:CGFloat = max(originMaxY, destinationMaxY)
+        let deltaX:CGFloat = maxX - minX
+        let deltaY:CGFloat = maxY - minY
+        
+        let marginedX:CGFloat = minX - kMargin
+        let marginedY:CGFloat = minY - kMargin
+        let widthMargin:CGFloat = deltaX + margin2
+        let heightMargin:CGFloat = deltaY + margin2
+        
+        let rect:CGRect = CGRect(
+            x:marginedX,
+            y:marginedY,
+            width:widthMargin,
+            height:heightMargin)
+        
+        link.draw(
+            rect:rect,
+            context:context,
+            zoom:kZoom,
+            selected:kSelected)
+    }
 }
