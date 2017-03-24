5ab115fea672b21559e0f3ec95d3c4766acef34b
vauxhall <zero.griffin@gmail.com>
2017-03-23 18:22:55 -0600

    share layout

--- cartesian/View/Settings/VSettingsCellShare.swift
+++ cartesian/View/Settings/VSettingsCellShare.swift
@@ -2,11 +2,52 @@ import UIKit
 
 class VSettingsCellShare:VSettingsCell
 {
+    private let kLabelLeft:CGFloat = 10
+    private let kLabelWidth:CGFloat = 250
+    private let kImageWidth:CGFloat = 90
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.backgroundColor = UIColor.clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.bold(size:15)
+        label.textColor = UIColor.black
+        label.text = NSLocalizedString("VSettingsCellShare_label", comment:"")
         
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.image = #imageLiteral(resourceName: "assetGenericShareApp")
+        
+        addSubview(label)
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:label,
+            toView:self,
+            constant:kLabelLeft)
+        NSLayoutConstraint.width(
+            view:label,
+            constant:kLabelWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageView,
+            constant:kImageWidth)
     }
     
     required init?(coder:NSCoder)
