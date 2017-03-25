d29b427ff478f3643d19fa6f8b43f731b6fb2b99
vauxhall <zero.griffin@gmail.com>
2017-03-24 18:00:05 -0600

    animate project store

--- cartesian/View/DrawProject/Store/VDrawProjectStore.swift
+++ cartesian/View/DrawProject/Store/VDrawProjectStore.swift
@@ -4,6 +4,7 @@ class VDrawProjectStore:UIView
 {
     private weak var controller:CDrawProject!
     private weak var purchase:MDrawProjectMenuNodesItem!
+    private let kAnimationDuration:TimeInterval = 0.3
     
     init(
         controller:CDrawProject,
@@ -18,15 +19,70 @@ class VDrawProjectStore:UIView
         
         let blur:VBlur = VBlur.dark()
         
+        let baseButton:UIButton = UIButton()
+        baseButton.clipsToBounds = true
+        baseButton.backgroundColor = UIColor.clear
+        baseButton.translatesAutoresizingMaskIntoConstraints = false
+        baseButton.addTarget(
+            self,
+            action:#selector(self.actionClose(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(blur)
+        addSubview(baseButton)
         
         NSLayoutConstraint.equals(
             view:blur,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:baseButton,
             toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionClose(sender button:UIButton)
+    {
+        animateClose()
+    }
+    
+    //MARK: private
+    
+    private func animateClose()
+    {
+//        layoutBaseTop.constant = -kBaseHeight
+        
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+            { [weak self] in
+                
+                self?.alpha = 0
+                self?.layoutIfNeeded()
+            })
+        { [weak self] (done:Bool) in
+            
+            self?.removeFromSuperview()
+        }
+    }
+    
+    //MARK: public
+    
+    func animateShow()
+    { 
+//        layoutBaseTop.constant = 0
+        
+        UIView.animate(withDuration:kAnimationDuration)
+        { [weak self] in
+            
+            self?.blurContainer.alpha = 0.95
+            self?.layoutIfNeeded()
+        }
     }
 }
