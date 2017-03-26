9a05cba90bada20f79548aa5b65375fd11880c52
vauxhall <zero.griffin@gmail.com>
2017-03-25 19:03:27 -0600

    title to menu items

--- cartesian/Model/DrawProject/Menu/Nodes/MDrawProjectMenuNodesItem.swift
+++ cartesian/Model/DrawProject/Menu/Nodes/MDrawProjectMenuNodesItem.swift
@@ -4,9 +4,14 @@ class MDrawProjectMenuNodesItem
 {
     let icon:UIImage
     let entityName:String
+    let title:String
     let available:Bool
     
-    init(icon:UIImage, entityName:String, available:Bool)
+    init(
+        icon:UIImage,
+        entityName:String,
+        title:String,
+        available:Bool)
     {
         self.icon = icon
         self.entityName = entityName
