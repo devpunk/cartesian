23ed9ba55e11e9d86fb10ac942e1c6a2eabdd6b8
vauxhall <zero.griffin@gmail.com>
2017-03-26 16:34:50 -0600

    button back layout

--- cartesian/View/Store/VStoreBar.swift
+++ cartesian/View/Store/VStoreBar.swift
@@ -5,6 +5,7 @@ class VStoreBar:UIView
     private weak var controller:CStore!
     private let kBorderHeight:CGFloat = 1
     private let kContentTop:CGFloat = 20
+    private let kBackWidth:CGFloat = 40
     
     init(controller:CStore)
     {
@@ -12,6 +13,7 @@ class VStoreBar:UIView
         clipsToBounds = true
         backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
         
         let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.1))
         
@@ -32,6 +34,7 @@ class VStoreBar:UIView
             for:UIControlState.highlighted)
         buttonBack.imageView!.clipsToBounds = true
         buttonBack.imageView!.contentMode = UIViewContentMode.center
+        buttonBack.imageView!.tintColor = UIColor(white:0, alpha:0.1)
         buttonBack.addTarget(
             self,
             action:#selector(actionBack(sender:)),
@@ -61,6 +64,20 @@ class VStoreBar:UIView
         NSLayoutConstraint.equalsHorizontal(
             view:icon,
             toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonBack,
+            toView:self,
+            constant:kContentTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:buttonBack,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonBack,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonBack,
+            constant:kBackWidth)
     }
     
     required init?(coder:NSCoder)
