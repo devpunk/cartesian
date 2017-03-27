ad8a9fd5c8d91918a4df919dbc559a952a175d28
vauxhall <zero.griffin@gmail.com>
2017-03-27 11:39:41 -0600

    firebase storage

---
+++ cartesian/Firebase/Storage/FStorage.swift
@@ -0,0 +1,47 @@
+import Foundation
+import FirebaseStorage
+
+class FStorage
+{
+    enum Folder:String
+    {
+        case gallery = "gallery"
+    }
+    
+    private let reference:FIRStorageReference
+    private let kTenMegaBytes:Int64 = 10000000
+    
+    init()
+    {
+        reference = FIRStorage.storage().reference()
+    }
+    
+    //MARK: public
+    
+    func saveData(path:String, data:Data, completionHandler:@escaping((String?) -> ()))
+    {
+        let childReference:FIRStorageReference = reference.child(path)
+        childReference.put(
+            data,
+            metadata:nil)
+        { (metaData:FIRStorageMetadata?, error:Error?) in
+            
+            let errorString:String? = error?.localizedDescription
+            completionHandler(errorString)
+        }
+    }
+    
+    func loadData(path:String, completionHandler:@escaping((Data?, Error?) -> ()))
+    {
+        let childReference:FIRStorageReference = reference.child(path)
+        childReference.data(
+            withMaxSize:kTenMegaBytes,
+            completion:completionHandler)
+    }
+    
+    func deleteData(path:String, completionHandler:@escaping((Error?) -> ()))
+    {
+        let childReference:FIRStorageReference = reference.child(path)
+        childReference.delete(completion:completionHandler)
+    }
+}
