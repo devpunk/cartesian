949a32caef8d056f3c033d716ce6aa3e6af7cc13
vauxhall <zero.griffin@gmail.com>
2017-03-27 09:21:57 -0600

    purchase item triangle

---
+++ cartesian/Model/Store/Purchases/MStoreItemNodeTriangle.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class MStoreItemNodeTriangle:MStoreItem
+{
+    private let kStorePurchaseId:MStore.PurchaseId = "iturbide.cartesian.nodeTriangle"
+    
+    init()
+    {
+        let title:String = NSLocalizedString("MStoreItemNodeTriangle_title", comment:"")
+        let descr:String = NSLocalizedString("MStoreItemNodeTriangle_descr", comment:"")
+        let image:UIImage = #imageLiteral(resourceName: "assetNodeTriangle")
+        
+        super.init(
+            purchaseId:kStorePurchaseId,
+            title:title,
+            descr:descr,
+            image:image)
+    }
+    
+    override func purchaseAction()
+    {
+        MSession.sharedInstance.settings?.purchaseNodePentagon = true
+        DManager.sharedInstance?.save()
+    }
+    
+    override func validatePurchase() -> Bool
+    {
+        guard
+            
+            let purchased:Bool = MSession.sharedInstance.settings?.purchaseNodeTriangle
+            
+        else
+        {
+            return false
+        }
+        
+        return purchased
+    }
+}
