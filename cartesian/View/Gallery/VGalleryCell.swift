0b48d14b2b18791da27fb1025bab792666222005
vauxhall <zero.griffin@gmail.com>
2017-03-27 15:37:56 -0600

    view gallery header

---
+++ cartesian/View/Gallery/VGalleryCell.swift
@@ -0,0 +1,9 @@
+//
+//  VGalleryCell.swift
+//  cartesian
+//
+//  Created by zero on 3/27/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ cartesian/View/Gallery/VGalleryHeader.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class VGalleryHeader:UICollectionReusableView
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
