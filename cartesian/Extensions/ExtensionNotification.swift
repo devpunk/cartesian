9f3f567178c73dca5fd1f0497043184a82536eb2
vauxhall <zero.griffin@gmail.com>
2017-03-28 10:40:51 -0600

    gallery image loaded notifciation

--- cartesian/Extensions/ExtensionNotification.swift
+++ cartesian/Extensions/ExtensionNotification.swift
@@ -6,4 +6,5 @@ extension Notification
     static let labelDraw:Notification.Name = Notification.Name("labelDraw")
     static let linkDraw:Notification.Name = Notification.Name("linkDraw")
     static let listItemRendered:Notification.Name = Notification.Name("listItemRendered")
+    static let galleryImageLoaded:Notification.Name = Notification.Name("galleryImageLoaded")
 }
