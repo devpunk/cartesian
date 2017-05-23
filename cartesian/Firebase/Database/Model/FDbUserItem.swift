d7b958dd61c9320fcbd7e9838c12c248a66e31e4
vauxhall <zero.griffin@gmail.com>
2017-05-23 10:39:20 -0500

    refactor firebase

--- cartesian/Firebase/Database/Model/FDbUserItem.swift
+++ cartesian/Firebase/Database/Model/FDbUserItem.swift
@@ -1,9 +1,53 @@
-//
-//  FDbUserItem.swift
-//  cartesian
-//
-//  Created by zero on 5/23/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class FDbUserItem:FDbProtocol
+{
+    static let created:String = "created"
+    static let shouldPost:String = "shouldPost"
+    
+    let created:TimeInterval
+    let shouldPost:Bool
+    private let kShouldPost:Bool = true
+    private let kShouldNotPost:Bool = false
+    private let kNoTime:TimeInterval = 0
+    
+    init()
+    {
+        created = NSDate().timeIntervalSince1970
+        shouldPost = kShouldPost
+    }
+    
+    required init?(snapshot:Any)
+    {
+        let snapshotDict:[String:Any]? = snapshot as? [String:Any]
+        
+        if let created:TimeInterval = snapshotDict?[
+            FDbUserItem.created] as? TimeInterval
+        {
+            self.created = created
+        }
+        else
+        {
+            self.created = kNoTime
+        }
+        
+        if let shouldPost:Bool = snapshotDict?[
+            FDbUserItem.shouldPost] as? Bool
+        {
+            self.shouldPost = shouldPost
+        }
+        else
+        {
+            self.shouldPost = kShouldNotPost
+        }
+    }
+    
+    func json() -> Any?
+    {
+        let json:[String:Any] = [
+            FDbUserItem.created:created,
+            FDbUserItem.shouldPost:shouldPost]
+        
+        return json
+    }
+}
