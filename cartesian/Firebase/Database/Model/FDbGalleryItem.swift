4dc7f875b0fde777edeb33f064a3cf8070e4340b
vauxhall <zero.griffin@gmail.com>
2017-05-23 10:13:23 -0500

    fdb gallery

---
+++ cartesian/Firebase/Database/Model/FDbGalleryItem.swift
@@ -0,0 +1,9 @@
+//
+//  FDbGalleryItem.swift
+//  cartesian
+//
+//  Created by zero on 5/23/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ cartesian/Firebase/Database/Model/FDbUserItem.swift
@@ -0,0 +1,9 @@
+//
+//  FDbUserItem.swift
+//  cartesian
+//
+//  Created by zero on 5/23/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
